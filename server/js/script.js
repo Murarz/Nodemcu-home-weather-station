7ab331e7d93c7f5e739dc841d2762cceb8d1f85e
Murarz <olejnik.kamil98@wp.pl>
2017-05-01 11:48:39 +0200

    Update script.js

--- server/js/script.js
+++ server/js/script.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+    
+    $(".topnav").hide();
+    $(".topnav").show(1000);
+    
+    
+    $('.topnav a').hover(
+    function() {
+     $(this).stop().fadeTo('fast', 1);
+    },
+    function() {
+     $(this).stop().fadeTo('fast', .8); 
+    });
 
+    
+
+
+
+});
